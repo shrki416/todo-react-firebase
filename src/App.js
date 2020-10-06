4d5bd55f948e43871e26954b350ca7cb4451dc27
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2020-10-06 15:40:56 -0400

    Add basic functionality & styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Take dogs for walk",
+    "Take the rubbish out",
+  ]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>
+        Hello, World!{" "}
+        <span role="img" aria-label="emoji">
+          👍
+        </span>
+      </h1>
+      <form>
+        <FormControl>
+          <InputLabel>Write a todo</InputLabel>
+          <Input value={input} onChange={(e) => setInput(e.target.value)} />
+        </FormControl>
+        <Button
+          type="submit"
+          onClick={addTodo}
+          variant="contained"
+          color="primary"
+          disabled={!input}
         >
-          Learn React
-        </a>
-      </header>
+          Add Todo
+        </Button>
+      </form>
+
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
